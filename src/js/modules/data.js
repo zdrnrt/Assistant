2b777eeed8b8d86bc6634a2382243c618f624b87
Задорин Артем <artfan95@gmail.com>
2025-02-27 23:30:03 +0700

    Верстка раздела Данные

--- src/js/modules/data.js
+++ src/js/modules/data.js
@@ -1,3 +1,5 @@
+import { Modal } from "bootstrap/dist/js/bootstrap.min";
+
 window.dataOpen = function() {
   fetch('./src/html/data.html')
       .then(response => {
@@ -14,4 +16,11 @@ window.dataOpen = function() {
       });
 }
 
-dataOpen()
+dataOpen();
+
+window.dataFilter = function(event){
+    event.preventDefault();
+    const form = event.target;
+    const result = new FormData(form);
+    console.log(result);
+}
