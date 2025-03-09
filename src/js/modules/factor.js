84d7736d7de25f7a2e77393b7959ca1b404b1ecd
Задорин Артем <artfan95@gmail.com>
2025-03-09 23:17:05 +0700

    Доработки модуля Факторного анализа

--- src/js/modules/factor.js
+++ src/js/modules/factor.js
@@ -1,3 +1,5 @@
+import { Chart } from "chart.js";
+
 window.factorOpen = function() {
   fetch('./src/html/factor.html')
       .then(response => {
@@ -8,8 +10,54 @@ window.factorOpen = function() {
       })
       .then(html => {
           document.getElementById('content').innerHTML = html;
+          factorOosChartDraw();
+          factorOsChartDraw();
       })
       .catch(error => {
           console.error('Возникла проблема с операцией выборки:', error);
       });
+}
+
+window.factorOsChartDraw = function() {
+    const data = {
+        labels: ['Коммерция', 'Логистика', 'Маркетинг', 'Операция', 'ОТД', 'Прогнозирование/Планирование'],
+        datasets: [
+          {
+            label: 'Значение ',
+            data: [
+                5, 6, 25, 10, 29, 25
+            ],
+          }
+        ]
+      };
+    const config = {
+        type: "doughnut",
+        data: data,
+        // options: {
+            // responsive: true,
+        // },
+    };
+    window.osChart = new Chart(document.getElementById('osChart'), config)
+}
+
+window.factorOosChartDraw = function() {
+    const data = {
+        labels: ['Коммерция', 'Логистика', 'Маркетинг', 'Операция', 'ОТД', 'Прогнозирование/Планирование'],
+        datasets: [
+          {
+            label: 'Значение ',
+            data: [
+                13, 6, 8, 17, 31, 25
+            ],
+          }
+        ]
+      };
+    const config = {
+        type: "doughnut",
+        data: data,
+        // options: {
+            // responsive: true,
+        // },
+    };
+    window.osChart = new Chart(document.getElementById('oosChart'), config)
 }
