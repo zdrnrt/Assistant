4a9869d1638bf504ea2da98b6e3fdfe6632c15ea
Задорин Артем <artfan95@gmail.com>
2025-03-12 10:48:35 +0700

    Модуль Сценарии

--- src/js/modules/scenarios.js
+++ src/js/modules/scenarios.js
@@ -1,3 +1,5 @@
+import { Chart } from 'chart.js/auto'
+
 window.scenariosOpen = function() {
   fetch('./src/html/scenarios.html')
       .then(response => {
@@ -8,8 +10,65 @@ window.scenariosOpen = function() {
       })
       .then(html => {
           document.getElementById('content').innerHTML = html;
+          window.scenariosChartDraw();
       })
       .catch(error => {
           console.error('Возникла проблема с операцией выборки:', error);
       });
+}
+
+window.scenariosChartDraw = function() {
+    // возможно отдельная функция для отрисовки каждого графика
+    const dataChart = [1_868_880, 1_500_000, 1_300_000, 900_000, 700_000, 542_000];
+    const optionsChart = {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+                position: 'bottom',
+            },
+            title: {
+                display: false,
+            },
+        }
+    }
+    const labelsChart = ['20.1', '10.2', '21.2', '8.3', '20.3', '4.4'];
+
+    const fastChart = document.getElementById('scenarios-fast-chart');
+    const middleChart = document.getElementById('scenarios-middle-chart');
+    const normalChart = document.getElementById('scenarios-normal-chart');
+
+    const fast = new Chart(fastChart, {
+        type: 'bar',
+        data: {
+            labels: labelsChart,
+            datasets: [{
+                label: '',
+                data: dataChart
+            }],
+        },
+        options: optionsChart,
+    });
+    const middle = new Chart(middleChart, {
+        type: 'bar',
+        data: {
+            labels: labelsChart,
+            datasets: [{
+                label: '',
+                data: dataChart
+            }],
+        },
+        options: optionsChart,
+    });
+    const normal = new Chart(normalChart, {
+        type: 'bar',
+        data: {
+            labels: labelsChart,
+            datasets: [{
+                label: '',
+                data: dataChart
+            }],
+        },
+        options: optionsChart,
+    });
 }
