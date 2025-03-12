4959a36310e3622ba254679c2ee422f4a3fcacc7
Задорин Артем <artfan95@gmail.com>
2025-03-12 23:56:18 +0700

    Разделение блока Факторный анализ

---
+++ src/js/modules/factorAnalysis.js
@@ -0,0 +1,64 @@
+import { Chart } from "chart.js";
+
+window.factorAnalysisOpen = function() {
+  fetch('./src/html/factorAnalysis.html')
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Реакция сети' + response.statusText);
+          }
+          return response.text(); 
+      })
+      .then(html => {
+          document.getElementById('content').innerHTML = html;
+          factorOosChartDraw();
+          factorOsChartDraw();
+      })
+      .catch(error => {
+          console.error('Возникла проблема с операцией выборки:', error);
+      });
+}
+
+
+window.factorOsChartDraw = function() {
+    const data = {
+        labels: ['Коммерция', 'Логистика', 'Маркетинг', 'Операция', 'ОТД', 'Прогнозирование/Планирование'],
+        datasets: [
+          {
+            label: 'Значение ',
+            data: [
+                5, 6, 25, 10, 29, 25
+            ],
+          }
+        ]
+      };
+    const config = {
+        type: "doughnut",
+        data: data,
+        // options: {
+            // responsive: true,
+        // },
+    };
+    window.osChart = new Chart(document.getElementById('osChart'), config)
+}
+
+window.factorOosChartDraw = function() {
+    const data = {
+        labels: ['Коммерция', 'Логистика', 'Маркетинг', 'Операция', 'ОТД', 'Прогнозирование/Планирование'],
+        datasets: [
+          {
+            label: 'Значение ',
+            data: [
+                13, 6, 8, 17, 31, 25
+            ],
+          }
+        ]
+      };
+    const config = {
+        type: "doughnut",
+        data: data,
+        // options: {
+            // responsive: true,
+        // },
+    };
+    window.oosChart = new Chart(document.getElementById('oosChart'), config)
+}
---
+++ src/js/modules/factorSetting.js
@@ -0,0 +1,17 @@
+import { Chart } from "chart.js";
+
+window.factorSettingOpen = function() {
+  fetch('./src/html/factorSetting.html')
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Реакция сети' + response.statusText);
+          }
+          return response.text(); 
+      })
+      .then(html => {
+          document.getElementById('content').innerHTML = html;
+      })
+      .catch(error => {
+          console.error('Возникла проблема с операцией выборки:', error);
+      });
+}
