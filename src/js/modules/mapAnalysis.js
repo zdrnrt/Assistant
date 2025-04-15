0db0dadd14a30119501f36e26f4e4ee821e7ad11
Задорин Артем <artfan95@gmail.com>
2025-04-15 21:47:24 +0700

    Доработки функционала

--- src/js/modules/mapAnalysis.js
+++ src/js/modules/mapAnalysis.js
@@ -8,8 +8,17 @@ window.mapAnalysisOpen = function() {
       })
       .then(html => {
           document.getElementById('content').innerHTML = html;
+          mapAnalysisStats();
       })
       .catch(error => {
           console.error('Возникла проблема с операцией выборки:', error);
       });
+}
+
+function mapAnalysisStats() {
+    const table = document.getElementById('table');
+    const list = Array.from(table.querySelectorAll('[data-type="rate"]'));
+    document.getElementById('all').textContent = list.length;
+    document.getElementById('under').textContent = list.reduce((result, el) => { console.log(Number(el.textContent) < 2, Number(el.textContent)); return Number(el.textContent) < 2 ? result + 1 : result  }, 0)
+    document.getElementById('above').textContent = list.reduce((result, el) => { console.log(Number(el.textContent) > 2, Number(el.textContent)); return Number(el.textContent) > 4 ? result + 1 : result  }, 0)
 }
