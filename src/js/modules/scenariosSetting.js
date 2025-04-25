527de3fbed10683d8c12278e0695457fc23413dd
Задорин Артем <artfan95@gmail.com>
2025-04-25 10:35:59 +0700

    Доработки раздела Сценарии настройка

--- src/js/modules/scenariosSetting.js
+++ src/js/modules/scenariosSetting.js
@@ -1,17 +1,42 @@
 import { Chart } from 'chart.js/auto'
+import { moduleOpen } from '../tools'
+
 
 window.scenariosSettingOpen = function() {
-  fetch('./src/html/scenariosSetting.html')
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Реакция сети' + response.statusText);
-          }
-          return response.text(); 
-      })
-      .then(html => {
-          document.getElementById('content').innerHTML = html;
-      })
-      .catch(error => {
-          console.error('Возникла проблема с операцией выборки:', error);
-      });
+    moduleOpen('./src/html/scenariosSetting.html')
+    .then( () => {
+        scenariosSettingInit()
+    });
+}
+
+scenariosSettingOpen();
+
+function scenariosSettingInit() {
+    for (const form of document.querySelectorAll('.scenariosForm')){
+        form.addEventListener('submit', scenariosSettingFormSubmit);
+    }
+    for (const btn of document.querySelectorAll('.scenariosForm__edit')){
+        btn.addEventListener('click', scenariosSettingFormEdit);
+    }
+    for (const btn of document.querySelectorAll('[data-id="add"]')){
+        btn.addEventListener('click', scenariosSettingAdd);
+    }
+    
+}
+
+function scenariosSettingFormSubmit(event){
+    event.preventDefault();
+}
+
+function scenariosSettingFormEdit(event){
+    const form = event.target.closest('.scenariosForm');
+    form.classList.toggle('scenariosForm--edit');
+}
+
+function scenariosSettingAdd(event){
+    const form = event.target.closest('.modal-body');
+    const filter = form.querySelector('[data-id="filter"]').cloneNode(true);
+    const list = form.querySelector('[data-id="list"]');
+    filter.querySelector('[data-id="order"]').textContent = list.querySelectorAll('[data-id="filter"]').length + 1;
+    list.append(filter);
 }
